d34a1b021ba43e1a7259f79cbb24604174cf16dc
xphszc <xphszc@qq.com>
2017-09-05 14:21:54 +0800

    添加base.html,测试jinjia2

--- flask.py
+++ flask.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+	return "Hello World!"
 
 if __name__ == __main__:
 	app.run()
-	
